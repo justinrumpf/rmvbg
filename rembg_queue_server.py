4e23045bf622e27e5b95bdc7f1c1fc10732e66f5
Justin Rumpf <justin.rumpf@gmail.com>
2025-05-31 08:19:57 -0400

    Update rembg_queue_server.py

--- rembg_queue_server.py
+++ rembg_queue_server.py
@@ -14,7 +14,7 @@ from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 from datetime import datetime, timedelta
 
-# --- CREATE DIRECTORIES AT THE VERY TOP 123---
+# --- CREATE DIRECTORIES AT THE VERY TOP V1---
 UPLOADS_DIR_STATIC = "/workspace/uploads"
 PROCESSED_DIR_STATIC = "/workspace/processed"
 BASE_DIR_STATIC = "/workspace/rmvbg"
